3f00d04b38a227fad49e2430602f6b3eadd64339
David Andrés Valencia García <david.andres.2801@gmail.com>
2022-10-28 00:55:11 -0500

    Update PostgreSqlDAOFactory.java

--- Proyecto ExtraClase/Carpooling/Carpooling_Data/src/edu/uco/carpooling/data/daofactory/PostgreSqlDAOFactory.java
+++ Proyecto ExtraClase/Carpooling/Carpooling_Data/src/edu/uco/carpooling/data/daofactory/PostgreSqlDAOFactory.java
@@ -2,6 +2,10 @@ package edu.uco.carpooling.data.daofactory;
 
 import java.sql.Connection;
 
+import edu.uco.carpooling.crosscutting.exception.CrosscuttingCarpoolingException;
+import edu.uco.carpooling.crosscutting.exception.DataCarpoolingException;
+import edu.uco.carpooling.crosscutting.helper.SQLConnectionHelper;
+import edu.uco.carpooling.crosscutting.messages.Messages;
 import edu.uco.carpooling.data.dao.DriverDAO;
 import edu.uco.carpooling.data.dao.UserDAO;
 import edu.uco.carpooling.data.dao.VehicleDAO;
@@ -20,29 +24,59 @@ final class PostgreSqlDAOFactory extends DAOFactory{
 	@Override
 	protected void openConnection() {
 		connection = null;
+		//TODO realize the connection to postgreSQL
 	}
 
 	@Override
 	public void initTransaction() {
-		// TODO Auto-generated method stub
+		try {
+			SQLConnectionHelper.initTransaction(connection);
+		} catch (CrosscuttingCarpoolingException exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.PostgreSqlDAOFactory.TECHNICAL_PROBLEM_INIT_TRANSACTION, exception);
+		} catch (Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.PostgreSqlDAOFactory.TECHNICAL_INIT_TRANSACTION_UNEXPECTED_ERROR, exception);
+		}
 		
 	}
 
 	@Override
 	public void confirmTransaction() {
-		// TODO Auto-generated method stub
-		
+		try {
+			SQLConnectionHelper.commitTransaction(connection);
+		} catch (CrosscuttingCarpoolingException exception) {
+			throw DataCarpoolingException.createTechnicalException(
+					Messages.PostgreSqlDAOFactory.TECHNICAL_PROBLEM_CONFIRM_TRANSACTION, exception);
+		} catch (Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(
+					Messages.PostgreSqlDAOFactory.TECHNICAL_CONFIRM_TRANSACTION_UNEXPECTED_ERROR, exception);
+		}
 	}
 
 	@Override
 	public void cancelTransaction() {
-		// TODO Auto-generated method stub
-		
+		try {
+			SQLConnectionHelper.rollbackTransaction(connection);
+		} catch (CrosscuttingCarpoolingException exception) {
+			throw DataCarpoolingException.createTechnicalException(
+					Messages.PostgreSqlDAOFactory.TECHNICAL_PROBLEM_CANCEL_TRANSACTION, exception);
+		} catch (Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(
+					Messages.PostgreSqlDAOFactory.TECHNICAL_CANCEL_TRANSACTION_UNEXPECTED_ERROR, exception);
+		}
 	}
 
 	@Override
 	public void closeConnection() {
-		// TODO Auto-generated method stub
+		try {
+			SQLConnectionHelper.closeConnection(connection);
+
+		} catch (CrosscuttingCarpoolingException exception) {
+			throw DataCarpoolingException.createTechnicalException(
+					Messages.PostgreSqlDAOFactory.TECHNICAL_PROBLEM_CLOSE_CONNECTION, exception);
+		} catch (Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(
+					Messages.PostgreSqlDAOFactory.TECHNICAL_CLOSE_CONNECTION_UNEXPECTED_ERROR, exception);
+		}
 		
 	}
 
