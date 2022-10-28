05aa38dcddbff523e3f3731491fa43abe807e9e6
David Andrés Valencia García <david.andres.2801@gmail.com>
2022-10-28 00:55:03 -0500

    Update Messages.java

--- Proyecto ExtraClase/Carpooling/Carpooling_Crosscutting/src/edu/uco/carpooling/crosscutting/messages/Messages.java
+++ Proyecto ExtraClase/Carpooling/Carpooling_Crosscutting/src/edu/uco/carpooling/crosscutting/messages/Messages.java
@@ -27,5 +27,32 @@ public class Messages {
 		
 		public static final String TECHNICAL_CONNECTION_IS_NULL="Connection is null";
 		public static final String TECHNICAL_CONNECTION_IS_CLOSED="Connection is closed";
+		public static final String TECHNICAL_CONNECTION_ALREDY_IS_CLOSED = "Connection alredy is closed";
+		public static final String TECHNICAL_PROBLEM_CLOSING_CONNECTION = "There was a problem try close the connection. Please verify the technicals details";
+		public static final String TECHNICAL_CONNECTION_IS_CLOSED_FOR_INIT_TRANSACTION = "Connection is closed to start a new transaction";
+		public static final String TECHNICAL_PROBLEM_TRY_INIT_TRANSACTION = "There was a problem trying to start the transaction. Please verify the technical details";	
+		public static final String TECHNICAL_CONNECTION_IS_CLOSED_FOR_COMMIT_TRANSACTION = "Connection is closed to commit the current transaction";
+		public static final String TECHNICAL_PROBLEM_TRY_COMMIT_TRANSACTION = "There was a problem trying to commit the current trasaction. Please verify te technical details";
+		public static final String TECHNICAL_CONNECTION_IS_CLOSED_FOR_ROLLBACK_TRANSACTION = "Connection is closed to rollback the current transaction";
+		public static final String TECHNICAL_PROBLEM_TRY_ROLLBACK_TRANSACTION = "There was a problem trying to rollback the current trasaction. Please verify te technical details";
+
+	}
+	
+	public static class PostgreSqlDAOFactory{
+		
+		private PostgreSqlDAOFactory() {
+			super();
+		}
+		
+		public static final String TECHNICAL_CONNECTION_NOT_POSSIBLE = "The connection is not possible";
+		public static final String TECHNICAL_OPEN_CONNECTION_UNEXPECTED_ERROR = "The was an unexpected error trying openning connection in PostgreSqlDAOFactory";
+		public static final String TECHNICAL_PROBLEM_INIT_TRANSACTION = "There was a problem trying to init transaction with the current connection in PostgreSqlDAOFactory";
+		public static final String TECHNICAL_INIT_TRANSACTION_UNEXPECTED_ERROR = "There was an unexpected error trying init transaction in PostgreSqlDAOFactory";
+		public static final String TECHNICAL_PROBLEM_CONFIRM_TRANSACTION = "There was a problem trying to confirm transaction with the current connection in PostgreSqlDAOFactory";
+		public static final String TECHNICAL_CONFIRM_TRANSACTION_UNEXPECTED_ERROR = "There was an unexpected error trying confirm transaction in PostgreSqlDAOFactory";
+		public static final String TECHNICAL_PROBLEM_CANCEL_TRANSACTION = "There was a problem trying to cancel transaction with the current connection in PostgreSqlDAOFactory";
+		public static final String TECHNICAL_CANCEL_TRANSACTION_UNEXPECTED_ERROR = "There was an unexpected error trying cancel transaction in PostgreSqlDAOFactory";
+		public static final String TECHNICAL_PROBLEM_CLOSE_CONNECTION = "There was a problem trying to close connection in PostgreSqlDAOFactory";
+		public static final String TECHNICAL_CLOSE_CONNECTION_UNEXPECTED_ERROR = "There was an unexpected error trying close connection in PostgreSqlDAOFactory";
 	}
 }
