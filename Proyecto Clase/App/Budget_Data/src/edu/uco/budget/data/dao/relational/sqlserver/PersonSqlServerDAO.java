2bf71cc338232c03dc63bbbfff24103c3450c2e4
David Andrés Valencia García <david.andres.2801@gmail.com>
2022-11-04 16:43:14 -0500

    implement methods (Person - Year) SqlServerDAO.java

--- Proyecto Clase/App/Budget_Data/src/edu/uco/budget/data/dao/relational/sqlserver/PersonSqlServerDAO.java
+++ Proyecto Clase/App/Budget_Data/src/edu/uco/budget/data/dao/relational/sqlserver/PersonSqlServerDAO.java
@@ -10,6 +10,7 @@ import edu.uco.budget.crosscutting.messages.Messages;
 import edu.uco.budget.data.dao.PersonDAO;
 import edu.uco.budget.data.dao.relational.DAORelational;
 import edu.uco.budget.domain.PersonDTO;
+import static edu.uco.budget.crosscutting.helper.UUIDHelper.getUUIDAsString;
 
 public final class PersonSqlServerDAO extends DAORelational implements PersonDAO {
 
@@ -51,18 +52,19 @@ public final class PersonSqlServerDAO extends DAORelational implements PersonDAO
 	@Override
 	public final void update(final PersonDTO person) {
 		final var sql = "UPDATE Person SET idCard = ?, firstName = ?, secondName = ?, firstSurname = ?, secondSurname = ? WHERE id = ?";
-		
-		try(final var preparedStatement = getConnection().prepareStatement(sql)) {
+
+		try (final var preparedStatement = getConnection().prepareStatement(sql)) {
 			preparedStatement.setString(1, person.getIdCard());
 			preparedStatement.setString(2, person.getFirstName());
 			preparedStatement.setString(3, person.getSecondName());
 			preparedStatement.setString(4, person.getFirstSurname());
 			preparedStatement.setString(5, person.getSecondSurname());
 			preparedStatement.setString(6, person.getIdAsString());
-			
+
 			preparedStatement.executeUpdate();
 		} catch (final SQLException exception) {
-			final String message = Messages.PersonSqlServerDAO.TECHNICAL_PROBLEM_UPDATE_PERSON.concat(person.getIdAsString());
+			final String message = Messages.PersonSqlServerDAO.TECHNICAL_PROBLEM_UPDATE_PERSON
+					.concat(person.getIdAsString());
 			throw DataBudgetException.createTechnicalException(message, exception);
 		} catch (final Exception e) {
 			// TODO: handle exception
@@ -71,7 +73,21 @@ public final class PersonSqlServerDAO extends DAORelational implements PersonDAO
 
 	@Override
 	public final void delete(final UUID id) {
-		// TODO Auto-generated method stub
+		final var sql = "DELETE FROM Person WHERE id = ?";
+		final var idAsString = getUUIDAsString(id);
+
+		try (final var preparedStatement = getConnection().prepareStatement(sql)) {
+
+			preparedStatement.setString(1, idAsString);
+
+			preparedStatement.executeUpdate();
+		} catch (final SQLException exception) {
+			final String message = Messages.PersonSqlServerDAO.TECHNICAL_PROBLEM_DELETE_PERSON.concat(idAsString);
+			throw DataBudgetException.createTechnicalException(message, exception);
+		} catch (final Exception exception) {
+			throw DataBudgetException.createTechnicalException(
+					Messages.PersonSqlServerDAO.TECHNICAL_UNEXPECTED_PROBLEM_DELETE_PERSON, exception);
+		}
 
 	}
 
