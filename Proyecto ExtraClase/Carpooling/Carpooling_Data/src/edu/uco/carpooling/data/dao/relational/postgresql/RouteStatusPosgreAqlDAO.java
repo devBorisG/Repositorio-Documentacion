eba44de6d34c96502500ff9ef8b9d942cc0ae4c0
Federico zapata <fede.zapata16@gmail.com>
2022-11-07 21:46:04 -0500

    DAO De estado ruta

--- Proyecto ExtraClase/Carpooling/Carpooling_Data/src/edu/uco/carpooling/data/dao/relational/postgresql/RouteStatusPosgreAqlDAO.java
+++ Proyecto ExtraClase/Carpooling/Carpooling_Data/src/edu/uco/carpooling/data/dao/relational/postgresql/RouteStatusPosgreAqlDAO.java
@@ -2,7 +2,9 @@ package edu.uco.carpooling.data.dao.relational.postgresql;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -44,8 +46,31 @@ public class RouteStatusPosgreAqlDAO extends DAORelational implements RouteStatu
 
 	@Override
 	public List<RouteStatusDTO> find(RouteStatusDTO status) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		var sqlBuilder = new StringBuilder();
+		final var parameters = new ArrayList<Object>();
+		
+		createSelectFrom(sqlBuilder);
+		createWhere(sqlBuilder, status, parameters);
+		createOrderBy(sqlBuilder);
+		
+		return prepareAndExecuteQuery(sqlBuilder, parameters);
+	}
+	
+	private final List<RouteStatusDTO> prepareAndExecuteQuery(final StringBuilder sqlBuilder, final List<Object> parameters) {
+		try (final var preparedStatement = getConnection().prepareStatement(sqlBuilder.toString())){
+			
+			setParameterValues(preparedStatement, parameters);
+			
+			return executeQuery(preparedStatement);
+			
+		} catch (final DataCarpoolingException exception) {
+			throw exception;
+		} catch (final SQLException exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_PROBLEM_PREPARED_STAMENT, exception);
+		} catch (final Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_UNEXPECTED_PROBLEM_SET_PARAMETER_VALUES_QUERY, exception);
+		}
 	}
 	
 	private final void createSelectFrom(final StringBuilder sqlBuilder) {
@@ -55,7 +80,7 @@ public class RouteStatusPosgreAqlDAO extends DAORelational implements RouteStatu
 		sqlBuilder.append("FROM        Status ST,");
 	}
 	
-	/*private final void createWhere(final StringBuilder sqlBuilder,final RouteStatusDTO status, final List<Object> parameters) {
+	private final void createWhere(final StringBuilder sqlBuilder,final RouteStatusDTO status, final List<Object> parameters) {
 		if(!ObjectHelper.isNull(status)) {
 			var setWhere = true;
 					
@@ -70,12 +95,74 @@ public class RouteStatusPosgreAqlDAO extends DAORelational implements RouteStatu
 				parameters.add(status.isStatus());
 			}
 			
-			if (StringHelper.applyTrim(status.getValueDefault())) {
+			if (!StringHelper.isEmpty(status.getValueDefault())) {
 				sqlBuilder.append(setWhere ? "WHERE ": "AND ").append("Bu.idPerson = ? ");
-				parameters.add(status);
+				parameters.add(status.getValueDefault());
 			}
 		}
-	}*/
+	}
+	
+	private void createOrderBy(final StringBuilder sqlBuilder) {
+		sqlBuilder.append("ORDER BY   St.idstaus ASC,");
+	}	
+	
+	private final List<RouteStatusDTO> executeQuery(PreparedStatement preparedStatement){
+		try (final var resultset = preparedStatement.executeQuery()){
+			return fillResults(resultset);	
+		}catch (final DataCarpoolingException exception) {
+			throw exception;
+		}catch (final SQLException exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_PROBLEM_EXECUTE_QUERY, exception);
+		}catch (final Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_UNEXPECTED_PROBLEM_EXECEUTE_QUERY, exception);
+		}
+	}
+	
+	private void setParameterValues(PreparedStatement preparedStatement, final List<Object> parameters) {
+		try {
+			for(int index = 0; index < parameters.size(); index++) {
+				preparedStatement.setObject(index + 1, parameters.get(index));
+			}
+		} catch (SQLException exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_PROBLEM_SET_PARAMETER_VALUES_QUERY, exception);
+		}catch (final Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_UNEXPECTED_PROBLEM_SET_PARAMETER_VALUES_QUERY, exception);
+		}
+	}
+	
+private final List<RouteStatusDTO> fillResults(final ResultSet resultSet){
+		
+		try {
+			var results = new ArrayList<RouteStatusDTO>();
+			
+			while(resultSet.next()) {
+				results.add(fillRouteStatusDTO(resultSet));
+			}
+			
+			return results;
+		} catch (final DataCarpoolingException exception) {
+			throw exception;
+		} catch (final SQLException exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_PROBLEM_FILL_RESULTS, exception);
+		}
+		catch (final Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_UNEXPECTED_PROBLEM_FILL_RESULTS, exception);
+		}
+	}
+
+	private final RouteStatusDTO fillRouteStatusDTO(final ResultSet resultset) {
+		try {
+			
+			return  RouteStatusDTO.create(resultset.getString("IdRiuteStatus"), resultset.getBoolean("Status"), resultset.getString("Messages"));
+			
+		} catch (final DataCarpoolingException exception) {
+			throw exception;
+		} catch (final SQLException exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_PROBLEM_FILL_ROUTE_ROUTE_STATUS_DTO, exception);
+		}catch (final Exception exception) {
+			throw DataCarpoolingException.createTechnicalException(Messages.RouteStatusPosgreAqlDAO.TECHNICAL_UNEXPECTED_PROBLEM_FILL_ROUTE_STATUS_DTO, exception);
+		}	
+	}
 
 	@Override
 	public final void update(final RouteStatusDTO status) {
